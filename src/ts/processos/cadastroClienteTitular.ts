909227e12b63d0c23478bbe2d7238b3c3f5fad16
Ana Júlia Rubim <anajrbcosta25@gmail.com>
2026-04-18 15:23:50 -0300

    Validação de nome vazio e data futura

--- src/ts/processos/cadastroClienteTitular.ts
+++ src/ts/processos/cadastroClienteTitular.ts
@@ -7,9 +7,25 @@ import CadastroEnderecoTitular from "./cadastroEnderecoTitular";
 export default class CadastroClienteTitular extends Processo {
     processar(): void {
         console.log('Iniciando o cadastro de um novo cliente...')
+
         let nome = this.entrada.receberTexto('Qual o nome do novo cliente?')
+        if (!nome || nome.trim() === '') {
+            console.log('Nome inválido. Cadastro cancelado.')
+            return
+        }
+
         let nomeSocial = this.entrada.receberTexto('Qual o nome social do novo cliente?')
+        if (!nomeSocial || nomeSocial.trim() === '') {
+            console.log('Nome social inválido. Cadastro cancelado.')
+            return
+        }
+
         let dataNascimento = this.entrada.receberData('Qual a data de nascimento?')
+        if (dataNascimento >= new Date()) {
+            console.log('Data de nascimento inválida. Cadastro cancelado.')
+            return
+        }
+
         let cliente = new Cliente(nome, nomeSocial, dataNascimento)
 
         this.processo = new CadastroEnderecoTitular(cliente)
@@ -18,9 +34,7 @@ export default class CadastroClienteTitular extends Processo {
         this.processo = new CadastrarDocumentosCliente(cliente)
         this.processo.processar()
 
-        let armazem = Armazem.InstanciaUnica
-        armazem.Clientes.push(cliente)
-
+        Armazem.InstanciaUnica.Clientes.push(cliente)
         console.log('Finalizando o cadastro do cliente...')
     }
 }
