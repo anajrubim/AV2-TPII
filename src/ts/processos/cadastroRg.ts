66a164b74f8a2d349934059d543a884118098f72
Ana Júlia Rubim <anajrbcosta25@gmail.com>
2026-04-18 15:21:50 -0300

    validação de RG

--- src/ts/processos/cadastroRg.ts
+++ src/ts/processos/cadastroRg.ts
@@ -1,4 +1,5 @@
 import Processo from "../abstracoes/processo";
+import Armazem from "../dominio/armazem";
 import { TipoDocumento } from "../enumeracoes/TipoDocumento";
 import Cliente from "../modelos/cliente";
 import Documento from "../modelos/documento";
@@ -12,8 +13,19 @@ export default class CadastroRg extends Processo {
 
     processar(): void {
         let numero = this.entrada.receberTexto('Qual o número do documento?')
+
+        let jaExiste = Armazem.InstanciaUnica.Clientes.some(c =>
+            c.Documentos.some(d => d.Tipo === TipoDocumento.RG && d.Numero === numero)
+        )
+
+        if (jaExiste) {
+            console.log(`Já existe um cliente cadastrado com o RG número ${numero}. Documento não adicionado.`)
+            return
+        }
+
         let dataExpedicao = this.entrada.receberData('Qual a data de expedição do documento?')
         let rg = new Documento(numero, TipoDocumento.RG, dataExpedicao)
         this.cliente.Documentos.push(rg)
+        console.log('RG cadastrado com sucesso!')
     }
 }
